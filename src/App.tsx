bd03d73a7fcd7ac8bf2a1b4bd989768bbbb10bae
narendar.baddam <narendar.baddam@opsramp.com>
2022-05-07 15:43:42 +0530

    initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from './components/Calendar';
 
 function App() {
+  const [showDayInfo, setShowDayInfo] = useState(false);
+  const [data, setData] = useState('');
+
+  const dayDetails = (dayStr:string) => {
+    setData(dayStr);
+    setShowDayInfo(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar dayDetails={dayDetails} />
+      {showDayInfo && <div>{data}</div>}
     </div>
-  );
-}
-
-export default App;
+  )
+  }
+export default App
